1122a0e8956ddde7e7dbce7e37dbf3e8f01549cd
birgit <birgit.tenerife@googlemail.com>
2026-02-24 13:39:56 +0000

    custom sale codes

---
+++ addons/dev/custom_sale_codes/__manifest__.py
@@ -0,0 +1,14 @@
+{ 
+    "name":"Custom Sale Codes (Report)",
+    "version": "1.0.0",
+    "category": "Sales",
+    "summary": "Anade referencia interno y codigo de barras al reporte de pedidos",
+    "depends": ["sales"],
+    "data": [
+        "views/sale_report_xml"
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGP-3"
+}
+   
